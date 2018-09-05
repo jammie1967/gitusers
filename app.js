f4bba7ae4d1f49bdcdbe5663aefe6e62992b7b71
jammie1967 <webuser@live.com.au>
2018-09-05 22:03:24 +1000

    All files up to date and working

--- app.js
+++ app.js
@@ -17,14 +17,16 @@ if(userText !== ''){
    .then(data => {
        if(data.profile.message === 'Not Found'){
            //Show alert
+           ui.showAlert('User not found', 'alert alert-danger');
 
        }else{
         // Show profile
         ui.showProfile(data.profile);
-
+        ui.showRepos(data.repos);
        }
    })
 }else{
   //Clear profile
+  ui.clearProfile();
 }
 });
--- github.js
+++ github.js
@@ -2,15 +2,26 @@ class Github{
     constructor(){
        this.client_id = '6ec21790eb9fa8490dfd';
        this.client_secret = '8aabeefdeec8521384dea770b059bab805d0dade';
+       this.repos_count = 5;
+       this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        
+        const repoResponse = await fetch
+        (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_count}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
         const profile = await profileResponse.json();
 
+       const repos = await repoResponse.json();
+
+
         return{
-            profile
+            profile,
+            repos
         }
     }
 }
